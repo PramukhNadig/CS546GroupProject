84b18e9fe42bacf8ddf2aa542f04ccfe2dec25ac
root <root@DESKTOP-UVOGOV5.localdomain>
2022-12-05 15:22:23 -0500

    A lot of database stuff sorted -- untested

---
+++ data/albums.js
@@ -0,0 +1,183 @@
+const mongodb = require('mongodb');
+const albumsDatabase = require('../config/mongoCollections').albums;
+const usersDatabase = require('../config/mongoCollections').users;
+const {
+    ObjectId
+} = require('mongodb');
+
+//SCHEMA FOR ALBUMS
+/* 
+    _id: ObjectId
+    title: String
+    artist: String
+    albumLength: String
+    reviews: subdocument
+    genre: String ! maybe should be an array
+    releaseYear: String
+    songs: array[ObjectId]
+*/
+
+const createAlbum = async (title, artist, albumLength, releaseYear, genre, songs) => {
+    if (!title || typeof title !== 'string') throw 'You must provide a title for your album';
+    if (!artist || typeof artist !== 'string') throw 'You must provide an artist for your album';
+    if (!albumLength || typeof albumLength !== 'string') throw 'You must provide an album length for your album';
+    if (!releaseYear || typeof releaseYear !== 'number') throw 'You must provide a release year for your album';
+    if (!genre || typeof genre !== 'string') throw 'You must provide a genre for your album';
+    if (!songs || !Array.isArray(songs)) throw 'You must provide an array of songs for your album';
+
+    const reviews = [];
+    const albumCollection = await albumsDatabase();
+
+    const newAlbum = {
+        title: title,
+        artist: artist,
+        albumLength: albumLength,
+        releaseYear: releaseYear,
+        genre: genre,
+        songs: songs,
+        reviews: reviews
+    };
+
+    const insertInfo = await albumCollection.insertOne(newAlbum);
+    if (insertInfo.insertedCount === 0) throw 'Could not add album';
+
+    const newId = insertInfo.insertedId;
+    const album = await this.getAlbumById(newId);
+    return album;
+}
+
+const getAllAlbums = async () => {
+    const albumCollection = await albumsDatabase();
+
+    const albums = await albumCollection.find({}).toArray();
+
+    return albums;
+}
+
+const getAlbumById = async (id) => {
+    if (!id) throw 'You must provide an id to search for';
+
+    const albumCollection = await albumsDatabase();
+    const parsedId = ObjectId(id);
+    const album = await albumCollection.findOne({
+        _id: parsedId
+    });
+    if (album === null) throw 'No album with that id';
+
+    return album;
+}
+
+const getAlbumsByArtist = async (artist) => {
+    if (!artist || typeof artist !== 'string') throw 'You must provide an artist to search for';
+
+    const albumCollection = await albumsDatabase();
+    const albums = await albumCollection.find({
+        artist: artist
+    }).toArray();
+
+    return albums;
+}
+
+const getAlbumsByGenre = async (genre) => {
+    if (!genre || typeof genre !== 'string') throw 'You must provide a genre to search for';
+
+    const albumCollection = await albumsDatabase();
+    const albums = await albumCollection.find({
+        genre: genre
+    }).toArray();
+
+    return albums;
+}
+
+const getAlbumsByTitle = async (title) => {
+    if (!title || typeof title !== 'string') throw 'You must provide a title to search for';
+
+    const albumCollection = await albumsDatabase();
+    const albums = await albumCollection.find({
+        title: title
+    }).toArray();
+
+    return albums;
+}
+
+const addReviewToAlbum = async (albumId, userId, review) => {
+    if (!albumId) throw 'You must provide an album id';
+    if (!userId) throw 'You must provide a user id';
+    if (!review || typeof review !== 'string') throw 'You must provide a review';
+
+    const albumCollection = await albumsDatabase();
+    const userCollection = await usersDatabase();
+    const parsedAlbumId = ObjectId(albumId);
+    const parsedUserId = ObjectId(userId);
+
+    const album = await albumCollection.findOne({
+        _id: parsedAlbumId
+    });
+    if (album === null) throw 'No album with that id';
+
+    const user = await userCollection.findOne({
+        _id: parsedUserId
+    });
+    if (user === null) throw 'No user with that id';
+
+    const newReview = {
+        userId: userId,
+        review: review
+    };
+
+    const updatedInfo = await albumCollection.updateOne({
+        _id: parsedAlbumId
+    }, {
+        $push: {
+            reviews: newReview
+        }
+    });
+    if (updatedInfo.modifiedCount === 0) throw 'Could not add review to album';
+
+    const updatedAlbum = await this.getAlbumById(albumId);
+    return updatedAlbum;
+};
+
+const updateAlbum = async (id, updatedAlbum) => {
+    if (!id) throw 'You must provide an id to search for';
+    if (!updatedAlbum) throw 'You must provide an album to update';
+
+    const albumCollection = await albumsDatabase();
+    const parsedId = ObjectId(id);
+
+    const updatedAlbumData = {};
+
+    if (updatedAlbum.title) {
+        updatedAlbumData.title = updatedAlbum.title;
+    }
+
+    if (updatedAlbum.artist) {
+        updatedAlbumData.artist = updatedAlbum.artist;
+    }
+
+    if (updatedAlbum.albumLength) {
+        updatedAlbumData.albumLength = updatedAlbum.albumLength;
+    }
+
+    if (updatedAlbum.releaseYear) {
+        updatedAlbumData.releaseYear = updatedAlbum.releaseYear;
+    }
+
+    if (updatedAlbum.genre) {
+        updatedAlbumData.genre = updatedAlbum.genre;
+    }
+
+    if (updatedAlbum.songs) {
+        updatedAlbumData.songs = updatedAlbum.songs;
+    }
+
+    let updateCommand = {
+        $set: updatedAlbumData
+    };
+    const query = {
+        _id: parsedId
+    };
+    await albumCollection.updateOne(query, updateCommand);
+
+    return await this.getAlbumById(id);
+}
---
+++ data/songs.js
@@ -0,0 +1,104 @@
+const mongodb = require('mongodb');
+const songsDatabase = require('../config/mongoCollections').songs;
+const usersDatabase = require('../config/mongoCollections').users;
+const {
+    ObjectId
+} = require('mongodb');
+
+const createSong = async (album, title, artist, songLength, releaseYear, genres, lyrics) => {
+    if (!album || typeof album !== 'string') throw 'You must provide an album name for your song';
+    if (!title || typeof title !== 'string') throw 'You must provide a title for your song';
+    if (!artist || typeof artist !== 'string') throw 'You must provide an artist for your song';
+    if (!songLength || typeof songLength !== 'string') throw 'You must provide a song length for your song';
+    if (!releaseYear || typeof releaseYear !== 'number') throw 'You must provide a release year for your song';
+    if (!genres || !Array.isArray(genres)) throw 'You must provide an array of genres for your song';
+    if (!lyrics || typeof lyrics !== 'string') throw 'You must provide lyrics for your song';
+
+    const reviews = [];
+    const songCollection = await songsDatabase();
+
+    const newSong = {
+        album: album,
+        title: title,
+        artist: artist,
+        songLength: songLength,
+        releaseYear: releaseYear,
+        genres: genres,
+        lyrics: lyrics,
+        reviews: reviews
+    };
+
+    const insertInfo = await songCollection.insertOne(newSong);
+    if (insertInfo.insertedCount === 0) throw 'Could not add song';
+
+    const newId = insertInfo.insertedId;
+    const song = await this.getSongById(newId);
+    return song;
+};
+
+const getAllSongs = async () => {
+    const songCollection = await songsDatabase();
+
+    const songs = await songCollection.find({}).toArray();
+
+    return songs;
+}
+
+const getSongById = async (id) => {
+    if (!id) throw 'You must provide an id to search for';
+
+    const songCollection = await songsDatabase();
+    const parsedId = ObjectId(id);
+    const song = await songCollection.findOne({
+        _id: parsedId
+    });
+    if (song === null) throw 'No song with that id';
+
+    return song;
+};
+
+const getSongsByArtist = async (artist) => {
+    if (!artist || typeof artist !== 'string') throw 'You must provide an artist to search for';
+
+    const songCollection = await songsDatabase();
+    const songs = await songCollection.find({
+        artist: artist
+    }).toArray();
+
+    return songs;
+};
+
+const getSongsByGenre = async (genre) => {
+    if (!genre || typeof genre !== 'string') throw 'You must provide a genre to search for';
+
+    const songCollection = await songsDatabase();
+    const songs = await songCollection.find({
+        genres: genre
+    }).toArray();
+
+    return songs;
+}
+
+const getSongsByTitle = async (title) => {
+    if (!title || typeof title !== 'string') throw 'You must provide a title to search for';
+
+    const songCollection = await songsDatabase();
+    const songs = await songCollection.find({
+        title: title
+    }).toArray();
+
+    return songs;
+}
+
+const getSongsWithMinAvgReview = async (avgReview) => {
+    if (!avgReview || typeof avgReview !== 'number') throw 'You must provide a number to search for';
+
+    const songCollection = await songsDatabase();
+    const songs = await songCollection.find({
+        avgReview: {
+            $gte: avgReview
+        }
+    }).toArray();
+
+    return songs;
+}
