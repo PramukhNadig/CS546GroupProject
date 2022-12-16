b24e63c5ae54b8538a68a3480d41a6a046533936
Justin O'Boyle <justin@justinoboyle.com>
2022-12-16 15:43:38 -0500

    Check for authentication

--- routes/songs.js
+++ routes/songs.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const data = require('../data');
+const data = require("../data");
 const songs = data.songs;
 const reviews = data.songReviews;
 const users = data.users;
@@ -9,72 +9,86 @@ const { UserError } = require("../helpers/userHelper");
 
 // for error handling
 const handleError = async (error, res) => {
-    if (!!error?._status) {
-      return res
-        .status(error._status)
-        .render("forbiddenAccess", { message: error.message });
-    }
-    console.error("Unhandled exception occured");
-    console.error(error);
+  if (!!error?._status) {
     return res
-      .status(500)
-      .render("forbiddenAccess", { message: "Internal server error" });
-  };
+      .status(error._status)
+      .render("forbiddenAccess", { message: error.message });
+  }
+  console.error("Unhandled exception occured");
+  console.error(error);
+  return res
+    .status(500)
+    .render("forbiddenAccess", { message: "Internal server error" });
+};
 
-router.route('/').get(async (req, res) => {
-    res.render('search');
+router.route("/").get(async (req, res) => {
+  res.render("search");
 });
 
-router.route('/:id').get(async (req, res) => {
+router
+  .route("/:id")
+  .get(async (req, res) => {
     try {
-        const song = await songs.getSongById(req.params.id);
-        song.id = song._id.toString()
-        const user = req.session?.user;
-        const songReviews = await reviews.getSongReviewBySongId(req.params.id);
-        
-        console.log(song);
-        console.log(user);
-        console.log(songReviews);
+      const song = await songs.getSongById(req.params.id);
+      song.id = song._id.toString();
+      const user = req.session?.user;
+      const songReviews = await reviews.getSongReviewBySongId(req.params.id);
 
-        res.status(200).render('song', {
-            song: song,
-            user: user ? user : "User not found",
-            songReviews: songReviews,
-            hasSongReviews: songReviews.length > 0 ? true : false
-        });
-    } catch (e) {
-        return handleError(e, res);
-    }
-}).post(async (req, res) => {
-    try {
-        const username = req.body.newReviewUsernameInput;
-        const title = req.body.newReviewTitleInput;
-        const rating = Number(req.body.newReviewRatingInput);
-        const comment = req.body.newReviewCommentInput;
-        const songID = req.params.id;
-        const userID = req.session?.user.id;
+      console.log(song);
+      console.log(user);
+      console.log(songReviews);
 
-        // run createSongReview
-        const resp = await reviews.createSongReview(title, userID, songID, username, rating, comment);
-        const reviewId = resp._id.toString();
-
-        // add it to song
-        const song = await songs.addReviewToSong(songID, reviewId);
-
-        // if it worked, redeliver the webpage
-        song.id = songID;
-        const user = req.session?.user;
-        const songReviews = await reviews.getSongReviewBySongId(req.params.id); // should have new review
-
-        res.status(200).render('song', {
-            song: song,
-            user: user ? user : "User not found",
-            songReviews: songReviews,
-            hasSongReviews: songReviews.length > 0 ? true : false
-        });
+      res.status(200).render("song", {
+        song: song,
+        user: user ? user : "User not found",
+        songReviews: songReviews,
+        hasSongReviews: songReviews.length > 0 ? true : false,
+      });
     } catch (e) {
       return handleError(e, res);
     }
-});
+  })
+  .post(async (req, res) => {
+    try {
+      if (!req.session?.user)
+        return res.redirect(
+          "/auth/login?next=" + encodeURIComponent(req.originalUrl)
+        );
+      const username = req.body.newReviewUsernameInput;
+      const title = req.body.newReviewTitleInput;
+      const rating = Number(req.body.newReviewRatingInput);
+      const comment = req.body.newReviewCommentInput;
+      const songID = req.params.id;
+      const userID = req.session?.user.id;
+
+      // run createSongReview
+      const resp = await reviews.createSongReview(
+        title,
+        userID,
+        songID,
+        username,
+        rating,
+        comment
+      );
+      const reviewId = resp._id.toString();
+
+      // add it to song
+      const song = await songs.addReviewToSong(songID, reviewId);
+
+      // if it worked, redeliver the webpage
+      song.id = songID;
+      const user = req.session?.user;
+      const songReviews = await reviews.getSongReviewBySongId(req.params.id); // should have new review
+
+      res.status(200).render("song", {
+        song: song,
+        user: user ? user : "User not found",
+        songReviews: songReviews,
+        hasSongReviews: songReviews.length > 0 ? true : false,
+      });
+    } catch (e) {
+      return handleError(e, res);
+    }
+  });
 
 module.exports = router;
