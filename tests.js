bab090c8add6b3713ca53aa576f91a511932fde3
PramukhNadig <72940884+PramukhNadig@users.noreply.github.com>
2022-12-16 19:44:42 -0500

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,11 +1,2 @@
 // TODO add something here
-const users = require("./data/users");
 console.log("Tested successfully")
-
-const test = async () => {
-
-    
-    users.addFriend("yarocks1", "yarocks2");
-}
-
-test();
